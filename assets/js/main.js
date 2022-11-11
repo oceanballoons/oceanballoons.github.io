919b58c471c052a4a35d6b6f4ad632fc5c21d780
chekinfotech <99319959+chekinfotech@users.noreply.github.com>
2022-11-10 19:28:37 -0800

    v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Paradigm Shift by Pixelarity
+	Standout by Pixelarity
 	pixelarity.com | hello@pixelarity.com
 	License: pixelarity.com/license
 */
@@ -7,17 +7,18 @@
 (function($) {
 
 	var	$window = $(window),
-		$body = $('body');
+		$body = $('body'),
+		$header = $('#header'),
+		$banner = $('#banner');
 
 	// Breakpoints.
 		breakpoints({
-			default:   ['1681px',   null       ],
-			xlarge:    ['1281px',   '1680px'   ],
-			large:     ['981px',    '1280px'   ],
-			medium:    ['737px',    '980px'    ],
-			small:     ['481px',    '736px'    ],
-			xsmall:    ['361px',    '480px'    ],
-			xxsmall:   [null,       '360px'    ]
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
 		});
 
 	// Play initial animations on page load.
@@ -27,60 +28,101 @@
 			}, 100);
 		});
 
-	// Hack: Enable IE workarounds.
-		if (browser.name == 'ie')
-			$body.addClass('is-ie');
-
-	// Mobile?
-		if (browser.mobile)
-			$body.addClass('is-mobile');
-
 	// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				offset: 100
-			});
+		$('.scrolly').scrolly({
+			offset: function() {
+				return $header.height();
+			}
+		});
 
-	// Polyfill: Object fit.
-		if (!browser.canUse('object-fit')) {
+	// Header.
+		if ($banner.length > 0
+		&&	$header.hasClass('alt')) {
 
-			$('.image[data-position]').each(function() {
+			$window.on('resize', function() { $window.trigger('scroll'); });
 
-				var $this = $(this),
-					$img = $this.children('img');
-
-				// Apply img as background.
-					$this
-						.css('background-image', 'url("' + $img.attr('src') + '")')
-						.css('background-position', $this.data('position'))
-						.css('background-size', 'cover')
-						.css('background-repeat', 'no-repeat');
-
-				// Hide img.
-					$img
-						.css('opacity', '0');
-
-			});
-
-			$('.gallery > a').each(function() {
-
-				var $this = $(this),
-					$img = $this.children('img');
-
-				// Apply img as background.
-					$this
-						.css('background-image', 'url("' + $img.attr('src') + '")')
-						.css('background-position', 'center')
-						.css('background-size', 'cover')
-						.css('background-repeat', 'no-repeat');
-
-				// Hide img.
-					$img
-						.css('opacity', '0');
-
+			$banner.scrollex({
+				bottom:		$header.outerHeight(),
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
 			});
 
 		}
+
+	// Banner.
+		var $banner = $('#banner');
+
+		(function() {
+
+			// Settings.
+				var settings = {
+
+					// Images (in the format of 'url': 'alignment').
+						images: {
+							'images/slides/slide01.jpg': '0% 35%',
+							'images/slides/slide02.jpg': 'center',
+							'images/slides/slide03.jpg': 'center',
+							'images/slides/slide04.jpg': 'center'
+						},
+
+					// Delay.
+						delay: 6000
+
+				};
+
+			// Vars.
+				var	pos = 0, lastPos = 0,
+					$wrapper, $bgs = [], $bg,
+					k, v;
+
+			// Create BG wrapper, BGs.
+				$wrapper = $('<div class="bg" />')
+					.appendTo($banner);
+
+				for (k in settings.images) {
+
+					// Create BG.
+						$bg = $('<div />');
+							$bg.css('background-image', 'url("' + k + '")');
+							$bg.css('background-position', settings.images[k]);
+							$bg.appendTo($wrapper);
+
+					// Add it to array.
+						$bgs.push($bg);
+
+				}
+
+			// Main loop.
+				$bgs[pos].addClass('visible');
+				$bgs[pos].addClass('top');
+
+				// Bail if we only have a single BG or the client doesn't support transitions.
+					if ($bgs.length == 1)
+						return;
+
+				setInterval(function() {
+
+					lastPos = pos;
+					pos++;
+
+					// Wrap to beginning if necessary.
+						if (pos >= $bgs.length)
+							pos = 0;
+
+					// Swap top images.
+						$bgs[lastPos].removeClass('top');
+						$bgs[pos].addClass('visible');
+						$bgs[pos].addClass('top');
+
+					// Hide last image after a short delay.
+						setTimeout(function() {
+							$bgs[lastPos].removeClass('visible');
+						}, settings.delay / 2);
+
+				}, settings.delay);
+
+		})();
 
 	// Gallery.
 		$('.gallery')
@@ -146,13 +188,13 @@
 
 				// Clear visible, loaded.
 					$modal
-						.removeClass('loaded')
+						.removeClass('loaded');
 
 				// Delay.
 					setTimeout(function() {
 
 						$modal
-							.removeClass('visible')
+							.removeClass('visible');
 
 						setTimeout(function() {
 
@@ -204,5 +246,111 @@
 						}, 275);
 
 					});
+
+	// Menu.
+		var $menu = $('#menu');
+
+		$menu._locked = false;
+
+		$menu._lock = function() {
+
+			if ($menu._locked)
+				return false;
+
+			$menu._locked = true;
+
+			window.setTimeout(function() {
+				$menu._locked = false;
+			}, 350);
+
+			return true;
+
+		};
+
+		$menu._show = function() {
+
+			if ($menu._lock())
+				$body.addClass('is-menu-visible');
+
+		};
+
+		$menu._hide = function() {
+
+			if ($menu._lock())
+				$body.removeClass('is-menu-visible');
+
+		};
+
+		$menu._toggle = function() {
+
+			if ($menu._lock())
+				$body.toggleClass('is-menu-visible');
+
+		};
+
+		$menu
+			.appendTo($body)
+			.on('click', function(event) {
+
+				event.stopPropagation();
+
+				// Hide.
+					$menu._hide();
+
+			})
+			.find('.inner')
+				.on('click', '.close', function(event) {
+
+					event.preventDefault();
+					event.stopPropagation();
+					event.stopImmediatePropagation();
+
+					// Hide.
+						$menu._hide();
+
+				})
+				.on('click', function(event) {
+					event.stopPropagation();
+				})
+				.on('click', 'a', function(event) {
+
+					var href = $(this).attr('href');
+
+					event.preventDefault();
+					event.stopPropagation();
+
+					// Hide.
+						$menu._hide();
+
+					// Redirect.
+						window.setTimeout(function() {
+							window.location.href = href;
+						}, 350);
+
+				});
+
+		$body
+			.on('click', 'a[href="#menu"]', function(event) {
+
+				event.stopPropagation();
+				event.preventDefault();
+
+				// Toggle.
+					$menu._toggle();
+
+			})
+			.on('keydown', function(event) {
+
+				// Hide on escape.
+					if (event.keyCode == 27)
+						$menu._hide();
+
+			});
+
+	// Tabs.
+		$('.tabs').selectorr({
+			titleSelector: 'h3',
+			delay: 250
+		});
 
 })(jQuery);
